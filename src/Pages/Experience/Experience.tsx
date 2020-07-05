81dad56cd120a0b12fe6d97da22049da878da0d7
Dhriaj Sriram <dhirajsriram3194@gmail.com>
2020-07-05 13:48:39 +0200

    Adding Everphone - Added job description - changed bold-text for all selectors - transition for background

--- src/Pages/Experience/Experience.tsx
+++ src/Pages/Experience/Experience.tsx
@@ -15,8 +15,13 @@ const Experience: React.FC = () => {
       image={require('../../assets/everphone-logo.webp')}
       devrole="Senior Frontend Engineer"
       duties={[
-        'Worked exclusively on the customer portal. <span class=`bold-text`> Added functionality, performance improvements, improved usability.</span>',
-        'Was invvolved in reviewing the CI/CD pipeling (github actions). Adding the neccessary steps'
+        'Worked exclusively on the customer portal. <span class="bold-text"> Added functionality, performance improvements, improved usability.</span>',
+        'Was involved in reviewing and improving the CI/CD pipeline (github actions). Involved in adding in the necessary automation steps which would <span class="bold-text">improve the overall quality and reliability of the portal</span>',
+        'Actively involved in consulting <span class="bold-text">Quality of life</span> improvements to the codebase to improve the overall developer experience',
+        'Involved in actively looking at some of the <span class="bold-text">state-of-the-art</span> technologies that would help with the performance, maintenance and reliability',
+        'Added unit tests as part of the development cycle to ensure that the solutions that we build are <span class="bold-text">reliable</span>',
+        'Involved in coordinating with the design, development and product teams so that the features we build are <span class="bold-text">feasible and empowers usability</span> among our customers',
+        'Actively involved in setting up the necessary tools to <span class="bold-text">analyze the experience of our customers which would inturn feeds into the concious desicisons</span> that we make towards the product'
       ]}
       duration={'Jan 2020 - Apr 2020 | Berlin, DE'}
       technologies={[
@@ -25,6 +30,8 @@ const Experience: React.FC = () => {
         'fab fa-html5',
         'fab fa-css3',
         'fab fa-sass',
+        'fab fa-github',
+        'fab fa-docker',
         'fab fa-js-square'
       ]}
     />
@@ -33,12 +40,12 @@ const Experience: React.FC = () => {
       image={require('../../assets/saucelabs-logo.webp')}
       devrole="Senior Frontend Engineer"
       duties={[
-        'Worked with Design and Product teams to develop features that offers <span class=`bold-text`>Enhancements, Sustainability, Accessibility</span> to the  platform.',
-        'Was involved in developing reusable packages for the <span class=`bold-text`>Component library</span> which gets used internally across various platforms.',
-        'Improved the quality and coverage of unit tests and E2E test which improved the overall <span class=`bold-text`>quality of the platform.</span>',
+        'Worked with Design and Product teams to develop features that offers <span class="bold-text">Enhancements, Sustainability, Accessibility</span> to the  platform.',
+        'Was involved in developing reusable packages for the <span class="bold-text">Component library</span> which gets used internally across various platforms.',
+        'Improved the quality and coverage of unit tests and E2E test which improved the overall <span class="bold-text">quality of the platform.</span>',
         'Was constantly involved in making technical decisions which would impact the productivity of various development teams.',
         'Was responsible for reviewing code which needs to adhere to Front-end standards, Efficiency and Reusability.',
-        'Was involved in developing A/B tests to make concious design decisions based on the user response.'
+        'Was involved in developing A/B tests to make conscious design decisions based on the user response.'
       ]}
       duration={'Jan 2020 - Apr 2020 | Berlin, DE'}
       technologies={[
@@ -55,12 +62,12 @@ const Experience: React.FC = () => {
       image={require('../../assets/taistech-logo.webp')}
       devrole="UI developer"
       duties={[
-        `Involved in designing HTML's and providing functionality to the web pages based on the requirements`,
-        'Responsible for building applications optimized for <span class=`bold-text`>speed,accessibility and responsiveness</span>',
+        "Involved in designing HTML's and providing functionality to the web pages based on the requirements",
+        'Responsible for building applications optimized for <span class="bold-text">speed,accessibility and responsiveness</span>',
         'Ensure the technical feasibility of UI/UX designs',
         'Collaborate with other team members and ensuring ceaseless development by helping them out in case of any technical blockades.',
         'Responsible for ensuring that the developed code adhere to the development standards with proper implementation of reusable libraries wherever necessary.',
-        'Ensuring that the code is <span class=`bold-text`>reusable and scalable for any future development.</span>'
+        'Ensuring that the code is <span class="bold-text">reusable and scalable for any future development.</span>'
       ]}
       duration={'Sep 2017 - Present | Addison, Texas'}
       technologies={[
@@ -79,8 +86,8 @@ const Experience: React.FC = () => {
       duties={[
         'Designed and Translated wireframes/PSDs into launch-ready, valid HTML/CSS of websites twghomeremodeling.com, twgcompanies.com, pickboxx.com',
         'Entirely designend and developed the website Lachlist.com.',
-        'Lead the design and development of Inutive apllications which ensures <span class=`bold-text`>ease-of-use and promotes user interaction</span>',
-        'Work with QA to ensure that the <span class=`bold-text`>applications meet desired objectives</span>',
+        'Lead the design and development of Inutive apllications which ensures <span class="bold-text">ease-of-use and promotes user interaction</span>',
+        'Work with QA to ensure that the <span class="bold-text">applications meet desired objectives</span>',
         'Ensured cross-browser and mobile compatibility(Responsive design). '
       ]}
       duration={'Jun 2016 - Sep 2017 | Newark, New jersey'}
